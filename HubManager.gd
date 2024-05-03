44d0f16b78c08a05825192218979a86755fd4ee5
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-05-02 20:21:48 -0700

    dwfregrwg

--- HubManager.gd
+++ HubManager.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var tempSave = $"/root/FileSave"
 @onready var data = $"/root/FileSave".piData
 @onready var dropdown = $"options/drop down"
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +16,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_new_button_up():
+	var max = len(data["profiles"])
+	print(max)
+	data["profiles"][str(max)] = {"name":("Profile "+str(max)),"1":{},"2":{},"3":{},"4":{},"5":{},"6":{},"7":{},"8":{}}
+	get_tree().reload_current_scene()
