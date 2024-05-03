54b7d7673dd6360a2a85ab19f6533fca8d10ce84
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-05-02 20:07:48 -0700

    oityjtr

--- HubManager.gd
+++ HubManager.gd
@@ -1,9 +1,15 @@
 extends Control
 
 @onready var data = $"/root/FileSave".piData
+@onready var dropdown = $"options/drop down"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$info.set_text(data["sku"]+" - v"+data["version"])
+	var num = 0
+	for each in data["profiles"]:
+		var text = data["profiles"][each]["name"]
+		dropdown.add_item(text,num)
+		num+=1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
